54779b28aa95b570e11f6782d671c27066b7822c
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-11-20 16:33:00 +0700

    chore: add interface usage

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Interfaces\RoleInterface;
 use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +12,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @mixin Eloquent
  * @mixin Builder
  */
-class Role extends Model
+class Role extends Model implements RoleInterface
 {
   protected $fillable = [
     'name',
