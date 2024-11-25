43c027c1fba6764532fe7e98d815cd369079132a
VARD0V <dimadima09876@gmail.com>
2024-11-25 15:56:30 +0700

    feat: add MAUI apps

---
+++ AdminPanel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdminPanel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UserPanel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UserPanel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
