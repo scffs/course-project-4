92a0b7dc32d4e66d3e2f85000efd0ad1e5e68a2d
VARD0V <dimadima09876@gmail.com>
2024-12-04 17:12:06 +0700

    fix

---
+++ UserPanel/Models/Auth.cs
@@ -0,0 +1,13 @@
+namespace UserPanel.Models;
+
+public class LoginRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
+public class AuthResponse
+{
+    public User User { get; set; }
+    public string Token { get; set; }
+}
---
+++ UserPanel/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace UserPanel.Models;
+
+public class User
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronymic { get; set; }
+    public bool Sex { get; set; }
+    public DateTime Birthday { get; set; }
+    public string Status { get; set; }
+    public string Login { get; set; }
+    public string AvatarUrl { get; set; }
+    public string RoleId { get; set; }
+}
