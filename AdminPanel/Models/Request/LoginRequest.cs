720aebe2598e4324831ca5ba9d321bf8b7072674
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-12-07 16:13:41 +0700

    format

--- AdminPanel/Models/Request/LoginRequest.cs
+++ AdminPanel/Models/Request/LoginRequest.cs
@@ -4,9 +4,9 @@ namespace AdminPanel.Models.Request;
 
 public class LoginRequest(string username, string password)
 {
-    [JsonPropertyName("username")]
-    public string Username { get; set; } = username ?? throw new ArgumentNullException(nameof(username));
+  [JsonPropertyName("username")]
+  public string Username { get; set; } = username ?? throw new ArgumentNullException(nameof(username));
 
-    [JsonPropertyName("password")]
-    public string Password { get; set; } = password ?? throw new ArgumentNullException(nameof(password));
+  [JsonPropertyName("password")]
+  public string Password { get; set; } = password ?? throw new ArgumentNullException(nameof(password));
 }
