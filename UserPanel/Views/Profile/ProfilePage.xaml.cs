19fb96f4eec8ad913381bc58f99e886efaf52059
VARD0V <dimadima09876@gmail.com>
2024-12-11 15:33:33 +0700

    add profile

---
+++ UserPanel/Views/Profile/ProfilePage.xaml.cs
@@ -0,0 +1,22 @@
+namespace UserPanel.Views.Profile;
+
+public partial class ProfilePage
+{
+  public ProfilePage()
+  {
+    InitializeComponent();
+  }
+
+  private async void OnLogoutClicked(object sender, EventArgs e)
+  {
+    try
+    {
+      Preferences.Remove("auth_token");
+      await Shell.Current.GoToAsync("//LoginPage");
+    }
+    catch (Exception err)
+    {
+      Console.WriteLine(err);
+    }
+  }
+}
