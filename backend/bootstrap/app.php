bcda7167a94cc1c5de52f3466ec2b5c285062152
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2024-12-21 14:33:53 +0700

    change api prefix

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     api: __DIR__ . '/../routes/api.php',
     commands: __DIR__ . '/../routes/console.php',
     health: '/up',
+    apiPrefix: 'encyclopedia',
   )
   ->withMiddleware(function (Middleware $middleware) {
     $middleware->redirectGuestsTo(fn() => throw new UnauthorizedApiException());
@@ -31,6 +32,6 @@ return Application::configure(basePath: dirname(__DIR__))
     });
 
     $exceptions->shouldRenderJsonWhen(
-      fn(Request $request) => $request->is('api/*') && !config('app.debug')
+      fn(Request $request) => $request->is('encyclopedia/*') && !config('app.debug')
     );
   })->create();
